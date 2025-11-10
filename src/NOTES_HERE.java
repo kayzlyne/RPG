36cf6dbdd2ce25e645b5c8dd43527db61cfc38a8
Dreya <dreyalabajos@gmail.com>
2025-11-10 19:39:09 +0800

    Revise notes on player death and battle system changes
    Updated notes regarding player death events and battle system parameters.

--- src/NOTES_HERE.java
+++ src/NOTES_HERE.java
@@ -17,8 +17,12 @@ public class NOTES_HERE {
         -pauses each dialogue
         -design and stuff (including alignment)
     -Insert confirmation after name input [✅ DONE -ef]
-    -What happens when player dies (Sir Khai event) (✅ DONE -a) //triggers everytime player dies between World 1 and 2 but will only trigger once in World 3
+    -What happens when player dies (Sir Khai event) (✅ DONE -a) 
+            -> //triggers everytime player dies between World 1 and 2 but will only trigger once in World 3
 
+        (guys I added an extra parameter when calling the battlesystem. (currentworld) kay need i keep track so that if naas world 3 sir khai can 
+        only revive once, but unli sa world 1 and 2) -a
+        (changed the methods with fight scenes from void to bool to continuously check if buhi pa ang player and end the game if di) -a
 
     For the story transfer, Make appropriate file/package.
 
